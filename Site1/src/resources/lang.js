c5fa9653b1bbf922bdd8f32297e2f10b00fd43d1
Natalya Pirogova <npirogova@phoenixit.ru>
2022-05-12 07:35:16 +0300

    site1

---
+++ Site1/src/resources/lang.js
@@ -0,0 +1,98 @@
+const langArr = {
+    "ways": {
+        "ru": "Решения и услуги",
+        "en": "Solutions and services"
+    },
+    "waystitle": {
+        "ru": "Решения и услуги",
+        "en": "Solutions and services"
+    },
+    "news": {
+        "ru": "Новости",
+        "en": "News"
+    },
+    "newstitle": {
+        "ru": "Новости",
+        "en": "News"
+    },
+    "vacation": {
+        "ru": "Вакансии",
+        "en": "Vacation"
+    },
+    "vacancytitle": {
+        "ru": "Вакансии",
+        "en": "Vacation"
+    },
+    "contacts": {
+        "ru": "Контакты",
+        "en": "Contacts"
+    },
+    "contactstitle": {
+        "ru": "Контакты",
+        "en": "Contacts"
+    },
+    "about__company": {
+        "ru": "О компании",
+        "en": "About us"
+    },
+    "text__company": {
+        "ru": "Наша компания объединияет опытных разработчиков, тестировщиков, дизайнеров и консультантов. Мы используем Web технологии для создания информационных решений и клиентского сервиса.",
+        "en": "Our company brings together experienced developers, testers, designers and consultants. We use Web technologies to create information solutions and customer service."
+    },
+    "textdelopingnews": {
+        "ru": "Данный раздел находится в разработке...",
+        "en": "This section is developing..."
+    },
+    "textdelopingvacancy": {
+        "ru": "Данный раздел находится в разработке...",
+        "en": "This section is developing..."
+    },
+    "ways1": {
+        "ru": "Разработка и доработка программных продуктов",
+        "en": "Development and improvement of software"
+    },
+    "ways2": {
+        "ru": "Тестирование ПО",
+        "en": "Testing of software"
+    },
+    "ways3": {
+        "ru": "Bеб-дизайн",
+        "en": "Web design"
+    },
+    "ways4": {
+        "ru": "Техническая поддержка<",
+        "en": "Technical support"
+    },
+    "bottom__company": {
+        "ru": "РЕШЕНИЯ И УСЛУГИ В ИНФОРМАЦИОННЫХ ТЕХНОЛОГИЯХ",
+        "en": "IT SOLUTIONS AND SERVICE"
+    },
+    "contact__us": {
+        "ru": "Связаться с нами",
+        "en": "Contact us"
+    },
+    "feedback": {
+        "ru": "Заказать обратный звонок",
+        "en": "Feedback"
+    },
+    "ordercall": {
+        "ru": "Заказать",
+        "en": "Call me"
+    },
+    "header__firstline": {
+        "ru": "ИНФОРМАЦИОННАЯ",
+        "en": "INFORMATION"
+    },
+    "header__secondline": {
+        "ru": "АРХИТЕКТУРА",
+        "en": "ARCHITECTURE"
+    },
+    "footer__firstline": {
+        "ru": "ИНФОРМАЦИОННАЯ",
+        "en": "INFORMATION"
+    },
+    "footer__secondline": {
+        "ru": "АРХИТЕКТУРА",
+        "en": "ARCHITECTURE"
+    },
+}
